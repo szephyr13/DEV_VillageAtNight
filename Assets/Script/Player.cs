612810070b53fab2d144d59a779b84fb3eda82be
Azazel <80421328+szephyr13@users.noreply.github.com>
2024-12-31 12:56:19 +0100

    player movement&animation fix

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,21 +8,68 @@ public class Player : MonoBehaviour
     private float inputH;
     [SerializeField] private float movementSpeed;
     [SerializeField] private float jumpForceValue;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputH = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(inputH * movementSpeed, rb.velocity.y);
+        MovementLogic();
+        JumpingLogic();
+        AttackLogic();
+    }
 
+
+
+
+    private void AttackLogic()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Attack");
+        }
+    }
+
+
+
+
+    private void JumpingLogic()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForceValue, ForceMode2D.Impulse);
+            anim.SetTrigger("Jump");
+        }
+    }
+
+
+
+
+    private void MovementLogic()
+    {
+        inputH = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(inputH * movementSpeed, rb.velocity.y);
+
+        if (inputH != 0)
+        {
+            anim.SetBool("Running", true);
+            if(inputH > 0)
+            {
+                transform.eulerAngles = Vector3.zero;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+        }
+        else
+        {
+            anim.SetBool("Running", false);
         }
     }
 }
